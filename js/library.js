5e7c23fd345c2b0afcb6f842737abf474f499020
FR33FA11 <dekoder.ai@pm.me>
2024-12-28 21:29:43 +0700

    Streamline code
     - Streamlined column creation through use of for loops
     - Added logic to avoid submitting books without all req data
     - Attempted to use dialog but encountered weird duplication bug

--- js/library.js
+++ js/library.js
@@ -17,14 +17,12 @@ const myLibrary = [
     new Book('Shantaram', 'Gregory David Roberts', 944, true),
     new Book('The Devil\'s Teeth', 'Susan Casey', 304, true)
 ];
-console.log(myLibrary);
 
-function Book(title, author, pages, read, spanIdNum) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.span = document.getElementById(`book-${spanIdNum}`);
     this.bookInfo = function() {
         let readText = null
         if (this.read === true) {
@@ -36,12 +34,6 @@ function Book(title, author, pages, read, spanIdNum) {
         return info
     }
 };
-
-
-function addBookToLibrary() {
- let x = 1;
-}
-
 
 // display new book form
 const NewBookForm = {
@@ -76,18 +68,55 @@ NewBookForm.body.addEventListener('click', (e) => {
             const title = document.getElementById("title").value;
             const author = document.getElementById("author").value;
             const pages = document.getElementById("pages").value;
-            const read = document.getElementById("read").value === 'true';
-            const book = new Book(title, author, pages, read);
-            myLibrary.push(book);
-            addRow(book);            
-            form.reset();
-            NewBookForm.closeForm();
+            if (title != '' && author != '' && pages != '') {
+                const book = new Book(title, author, pages,
+                    document.getElementById("read").value === 'true');
+                myLibrary.push(book);
+                addRow(book);            
+                form.reset();
+                NewBookForm.closeForm();
+                console.log(book);
+            } else { 
+                alert('Please complete all fields')
+            }
+
         case 'close-form':
             NewBookForm.closeForm();
         // default:
         //     NewBookForm.closeForm();
     } 
 });
+
+// open new book dialog
+// function addNewBook() {
+//     const newBookBtn = document.getElementById('new-book-btn');
+//     const dialog = document.getElementById('new-book-dialog');
+//     const body = document.querySelector('body');
+//     newBookBtn.addEventListener("click", () => {
+//         dialog.showModal();
+//         body.addEventListener('click', (e) => {
+//             let target = e.target;
+//             switch(target.id) {
+//             case 'submit-book-btn':
+//                 // get submitted form data
+//                 const form = document.getElementById('new-book-data');
+//                 // prevent form from submitting data
+//                 e.preventDefault();
+//                 const book = new Book(document.getElementById("title").value,
+//                     document.getElementById("author").value,
+//                     document.getElementById("pages").value,
+//                     document.getElementById("read").value === 'true');
+//                 myLibrary.push(book);
+//                 addRow(book);            
+//                 form.reset();
+//                 dialog.close();
+//             case 'close-form':
+//                 dialog.close();
+//             }
+//         });
+//     });
+// }
+// addNewBook();
 
 
 function createCheckbox(book) {
@@ -103,7 +132,6 @@ function createCheckbox(book) {
     }
     return checkbox;
 }
-
 function createDeleteBtn() {
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'Delete';
@@ -114,26 +142,21 @@ function createDeleteBtn() {
 function addRow(book) {
     const bookTable = document.getElementById('book-table');
     const newRow = document.createElement('tr');
-    const column01 = document.createElement('td');
-    const column02 = document.createElement('td');
-    const column03 = document.createElement('td');
-    column03.setAttribute('class', 'column-3-data')
-    const column04 = document.createElement('td');
-    column04.setAttribute('class', 'column-4-data')
-    const column05 = document.createElement('td');
+    let column = []
+    for (let i = 0; i < 5; i++) {column[i] = document.createElement('td');}
+    column[2].setAttribute('class', 'column-3-data')
+    column[3].setAttribute('class', 'column-4-data')
     const checkbox = createCheckbox(book);
     checkbox.setAttribute('class', 'check');
     const deleteBtn = createDeleteBtn();
-    column01.textContent = book.title;
-    column02.textContent = book.author;
-    column03.textContent = book.pages;
-    column04.appendChild(checkbox);
-    column05.appendChild(deleteBtn);
-    newRow.appendChild(column01);
-    newRow.appendChild(column02);
-    newRow.appendChild(column03);
-    newRow.appendChild(column04);
-    newRow.appendChild(column05);
+    column[0].textContent = book.title;
+    column[1].textContent = book.author;
+    column[2].textContent = book.pages;
+    column[3].appendChild(checkbox);
+    column[4].appendChild(deleteBtn);
+    for (let i = 0; i < 5; i++) {
+        newRow.appendChild(column[i]);
+    }
     bookTable.appendChild(newRow);
     deleteBtn.addEventListener('click', () => {
         newRow.remove();
@@ -145,9 +168,7 @@ function addRow(book) {
         } else {
           book.read= false;
         }
-        console.log(book);
     });
-      
 }
 
 // add books in array to DOM table
